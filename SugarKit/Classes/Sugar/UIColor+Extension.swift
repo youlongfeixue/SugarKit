2f200da683be7327346e99af097116a14d37bd0e
youlongfeixue <youlongfeixue@sina.com>
2024-06-26 09:33:00 +0800

    color

--- SugarKit/Classes/Sugar/UIColor+Extension.swift
+++ SugarKit/Classes/Sugar/UIColor+Extension.swift
@@ -9,13 +9,34 @@
 import UIKit
 
 public extension UIColor {
-
+    
+    class func random() -> UIColor {
+        return UIColor(red: CGFloat(arc4random_uniform(256)) / 255.0, 
+                       green: CGFloat(arc4random_uniform(256)) / 255.0, 
+                       blue: CGFloat(arc4random_uniform(256)) / 255.0, 
+                       alpha: 1.0)
+    }
+    
+    /// 根据 r/g/b 的数值创建 UIColor  
+    ///
+    /// - Parameters:
+    ///   - r: red
+    ///   - g: green
+    ///   - b: blue
+    ///   - alpna: alpha
+    convenience init(sk_red: CGFloat, sk_green: CGFloat, sk_blue: CGFloat, alpha: CGFloat = 1.0) {
+        self.init(red: sk_red / 255.0, green: sk_green / 255.0, blue: sk_blue / 255.0, alpha: 1.0)
+    }
+    
+    /// 根据 16进制 颜色创建 UIColor 对象 
+    ///
+    /// - Parameter hex: 16进制颜色 - 0xFFFFFF
     convenience init(hex: Int32, alpha: CGFloat = 1.0) {
         self.init(
             red:    CGFloat((hex & 0xFF0000) >> 16) / 255.0, 
             green:  CGFloat((hex & 0x00FF00) >> 8)  / 255.0, 
             blue:   CGFloat(hex & 0x0000FF) / 255.0, 
-            alpha: alpha
+            alpha:  alpha
         )
     }
 
@@ -51,6 +72,22 @@ public extension UIColor {
     /// 118, 166, 248
     class var system_Blue: UIColor { UIColor(red: 118, green: 166, blue: 248, alpha: 1) }
     
+    
+    /// 森林绿 34, 139, 34
+    class var forestGreen: UIColor {
+        return UIColor(red: 34.0 / 255.0, 
+                       green: 139.0 / 255.0, 
+                       blue: 34.0 / 255.0, 
+                       alpha: 1.0)
+    }
+    
+    /// 草绿色 153, 204, 51
+    class var grassGreen: UIColor {
+        return UIColor(red: 153.0 / 255.0, 
+                       green: 204.0 / 255.0, 
+                       blue: 51.0 / 255.0, 
+                       alpha: 1.0)
+    }
     
 }
 
